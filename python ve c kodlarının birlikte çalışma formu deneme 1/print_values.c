b9c43e59c8c30162dcbebaa6978fcd61ef658fdc
Muhammed Dinç <54439858+muhammeddincmdx@users.noreply.github.com>
2024-01-02 23:40:14 +0300

    Add files via upload

---
+++ python ve c kodlarının birlikte çalışma formu deneme 1/print_values.c
@@ -0,0 +1,6 @@
+// print_values.c
+#include <stdio.h>
+
+void print_values(int x, int y, int scale) {
+    printf("Received values: X=%d, Y=%d, Scale=%d\n", x, y, scale);
+}
---
+++ python ve c kodlarının birlikte çalışma formu deneme 1/readme.md
@@ -0,0 +1,2 @@
+bu dosya ctypes kullanılarak pythonda alınan değerlerin c ile kullanılmasını kapsayan ilk deneydir.
+gcc -shared -o libprintvalues.so print_values.c
